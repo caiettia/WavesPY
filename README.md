e3291fc8099208b91cfd362adbb637d08b7f540f
Andrew Caietti <67521582+caiettia@users.noreply.github.com>
2024-05-19 03:55:26 -0400

    new html file content

--- README.md
+++ README.md
@@ -18,3 +18,8 @@ This one tabular query provides the wave height statuses for a given region of b
 ### Buoy-Based Data
 #### Wave Height
 This tabular query provides a view of buoy data in for the current hour as well as historical data: https://www.ndbc.noaa.gov/data/realtime2/46222.spec
+
+
+
+## GUIDE
+Use terminal to navigate to the directory of the wavespy project. from there, run the local server at http-server -p 8000. To access the server from there, navigate to http://localhost:8000 to access the server!
--- script.js
+++ script.js
@@ -2,19 +2,48 @@ document.addEventListener('DOMContentLoaded', () => {
     console.log('Hello, world!');
 });
 
-document.querySelector('button').addEventListener('click', () => {
-    alert('Button clicked!');
-});
+document.querySelector('button').addEventListener('click', () => {alert('Button clicked!');});
 
+
+// MAP //
 // Initialize the map and set its view to a chosen geographical coordinates and zoom level
 var map = L.map('map').setView([32.8328, -117.2713], 12);
+
 
 // Add a tile layer to the map (OpenStreetMap in this case)
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '© OpenStreetMap contributors'
 }).addTo(map);
 
-// Add a marker to the map
-L.marker([32.8328, -117.2713]).addTo(map)
-    .bindPopup('Homebase.<br> We can go from here!')
-    .openPopup();
+var markersGroup = L.featureGroup().addTo(map);
+
+// Function to load and parse the CSV file
+function loadCSV() {
+    Papa.parse('current_buoy_data.csv', {
+        download: true,
+        header: true,
+        complete: function(results) {
+            console.log(results)
+            plotData(results.data);
+        }
+    });
+}
+
+// Function to plot data on the map
+function plotData(data) {
+    data.forEach(function(row) {
+        if (row.latitude && row.longitude) {
+            var popupContent = `
+                <strong>Name:</strong> ${row.NAME}<br>
+                <strong>Latitude:</strong> ${row.latitude}<br>
+                <strong>Longitude:</strong> ${row.longitude}<br>
+                <strong>Swell Direction:</strong> ${row.SwD}
+            `;
+            var marker = L.marker([row.latitude, row.longitude]).bindPopup(popupContent);
+            markersGroup.addLayer(marker);
+
+        }
+    });
+}
+
+loadCSV();
